84e7a4aee766490c207860f6d75a2ce8a370906c
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-05-20 17:00:00 -0300

    separated front in components

---
+++ aulas/web/src/components/Copyright.tsx
@@ -0,0 +1,17 @@
+export function Copyright() {
+    return (
+        <div>
+            <p className="text-sm leading-relaxed text-gray-200">
+                Feito com 💜 no NLW da{" "}
+                <a
+                    href="https://rocketseat.com.br"
+                    className="underline hover:text-gray-100"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Rocketseat
+                </a>
+            </p>
+        </div>
+    );
+}
---
+++ aulas/web/src/components/EmptyMemories.tsx
@@ -0,0 +1,15 @@
+export function EmptyMemories() {
+    return (
+        <div>
+            <p className="w-[360px] text-center leading-relaxed">
+                Você ainda não registrou nenhuma lembrança,
+                <br />
+                comece a{" "}
+                <a href="" className="underline hover:text-gray-50">
+                    criar agora
+                </a>
+                !
+            </p>
+        </div>
+    );
+}
