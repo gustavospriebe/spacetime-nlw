2e8600c11c0934d16cd743428df8e4813ec01206
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-06-04 22:56:32 -0300

    upload files

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -1,15 +1,24 @@
 import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
 import "dotenv/config";
 import fastify from "fastify";
+import { resolve } from "node:path";
 import { authRoutes } from "./routes/auth";
 import { memoriesRoutes } from "./routes/memories";
+import { uploadRoutes } from "./routes/upload";
 
 const app = fastify();
 
+app.register(require("@fastify/static"), {
+    root: resolve(__dirname, "../uploads"),
+    prefix: "/uploads",
+});
 app.register(memoriesRoutes);
+app.register(multipart);
 app.register(cors, { origin: true });
 app.register(authRoutes);
+app.register(uploadRoutes);
 app.register(jwt, { secret: "spacetime" });
 
 app.listen({
